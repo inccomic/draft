7655aa64e7d1961f964d4f65de14ac724766cd7c
inccomic <inccomic@foxmail.com>
2026-01-31 13:33:05 +0800

    基本的绘制功能

--- src/App/index.js
+++ src/App/index.js
@@ -4,6 +4,7 @@ import style from "./index.scss"
 
 import { Canvas } from "vislite"
 
+let painter
 export default defineElement({
     template,
     data() {
@@ -12,12 +13,51 @@ export default defineElement({
         }
     },
     methods: {
-
+        clear() {
+            painter.clear()
+        },
+        download() {
+            painter.toDataURL().then(dataURL => {
+                const link = document.createElement('a')
+                link.href = dataURL
+                link.download = 'drawing.png'
+                link.click()
+            });
+        },
+        colorInput(event){
+            painter.config({ strokeStyle: event.target.value })
+        },
+        sizeInput(event){
+            painter.config({ lineWidth: event.target.value })
+        }
     },
     created() {
-        let painter = new Canvas(document.getElementById("root"))
+        painter = new Canvas(document.getElementById('canvas-wrap'))
 
-        console.log(painter )
+        const colorInput = document.getElementById('color')
+        const sizeInput = document.getElementById('size')
+
+        // 初始化配置
+        painter.config({
+            strokeStyle: colorInput.value,
+            lineWidth: sizeInput.value
+        })
+
+        let isDrawing = false
+        painter.bind("mousedown", (_, x, y) => {
+            isDrawing = true
+            painter.beginPath()
+        })
+        painter.bind("mouseup", (_, x, y) => {
+            isDrawing = false
+            painter.closePath()
+        })
+        painter.bind("mousemove", (_, x, y) => {
+            if (!isDrawing) return
+            painter.lineTo(x, y)
+            painter.stroke()
+        })
+
     },
     style: {
         content: style
